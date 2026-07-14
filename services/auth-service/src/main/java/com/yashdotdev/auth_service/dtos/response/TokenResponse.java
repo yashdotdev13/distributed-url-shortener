1bef57d675b0dcd09d195c39751529847f6e9faa
98001yash <yashchauhan.gaya@gmail.com>
2026-07-14 22:34:02 +0530

    add: user response and token response

--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/TokenResponse.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/TokenResponse.java
@@ -1,4 +1,22 @@
 package com.yashdotdev.auth_service.dtos.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String tokenType;
+
+    private Long expiresId;
 }
--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/UserResponse.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/UserResponse.java
@@ -1,4 +1,27 @@
 package com.yashdotdev.auth_service.dtos.response;
 
+
+import com.yashdotdev.auth_service.enums.AccountStatus;
+import com.yashdotdev.auth_service.enums.AuthProvider;
+import com.yashdotdev.auth_service.enums.Role;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+    private Set<Role> roles;
+    private AccountStatus status;
+    private AuthProvider authProvider;
+    private boolean emailVerified;
+    private Instant createdAt;
 }
