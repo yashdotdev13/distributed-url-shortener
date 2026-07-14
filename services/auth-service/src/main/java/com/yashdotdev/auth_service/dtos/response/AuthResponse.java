a62949edfec551823e874a297c6cbf5f44fa89ce
98001yash <yashchauhan.gaya@gmail.com>
2026-07-14 22:37:03 +0530

    add: LoginResponse and authResponse dtos

--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/AuthResponse.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/AuthResponse.java
@@ -1,4 +1,15 @@
 package com.yashdotdev.auth_service.dtos.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthResponse {
+
+    private UserResponse user;
+
+    private TokenResponse token;
 }
--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/LoginResponse.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/response/LoginResponse.java
@@ -1,4 +1,16 @@
 package com.yashdotdev.auth_service.dtos.response;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResponse {
+
+    private UserResponse user;
+
+    private TokenResponse token;
 }
