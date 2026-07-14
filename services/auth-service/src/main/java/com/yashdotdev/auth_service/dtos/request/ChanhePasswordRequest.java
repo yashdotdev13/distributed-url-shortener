1f107631af67101b3ec03b0ce221dc51356e756d
98001yash <yashchauhan.gaya@gmail.com>
2026-07-14 22:25:40 +0530

    sdd: changePassowrdRequest and refreshToken request dto

--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/request/ChanhePasswordRequest.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/request/ChanhePasswordRequest.java
@@ -1,4 +1,22 @@
 package com.yashdotdev.auth_service.dtos.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChanhePasswordRequest {
+
+    @NotBlank
+    private String currentPassword;
+
+
+    @NotBlank
+    @Size(min=  8, max = 100)
+    private String newPassword;
 }
--- services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/request/RefreshTokenRequest.java
+++ services/auth-service/src/main/java/com/yashdotdev/auth_service/dtos/request/RefreshTokenRequest.java
@@ -1,4 +1,16 @@
 package com.yashdotdev.auth_service.dtos.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh token is required")
+    private String refreshToken;
 }
