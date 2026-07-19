3d531f7bb9f7cd8911c5c60acdaf831793bd0aa7
98001yash <yashchauhan.gaya@gmail.com>
2026-07-19 13:59:13 +0530

    added the Gateway constatns

--- services/api-gateway/src/main/java/com/yashdotdev/api_gateway/constants/GatewayConstants.java
+++ services/api-gateway/src/main/java/com/yashdotdev/api_gateway/constants/GatewayConstants.java
@@ -1,8 +1,19 @@
 package com.yashdotdev.api_gateway.constants;
 
-public  final class GatewayConstants {
+public final class GatewayConstants {
 
-    private GatewayConstants() {}
+    private GatewayConstants() {
+    }
 
     public static final String CORRELATION_ID = "X-Correlation-ID";
+
+    public static final String AUTHORIZATION = "Authorization";
+
+    public static final String BEARER = "Bearer ";
+
+    public static final String USER_ID = "X-User-Id";
+
+    public static final String USERNAME = "X-Username";
+
+    public static final String ROLES = "X-Roles";
 }
