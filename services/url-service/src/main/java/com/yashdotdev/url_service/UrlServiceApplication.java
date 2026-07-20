27424363583aa1879ae3c86584a97d7932c51c98
98001yash <yashchauhan.gaya@gmail.com>
2026-07-20 11:36:25 +0530

    url-service added

---
+++ services/url-service/src/main/java/com/yashdotdev/url_service/UrlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.url_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlServiceApplication.class, args);
+	}
+
+}
---
+++ services/url-service/src/test/java/com/yashdotdev/url_service/UrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.url_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
