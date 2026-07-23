14bd5cea6712d6f9dcaf75f32757fa0600d6b8bd
98001yash <yashchauhan.gaya@gmail.com>
2026-07-23 12:51:37 +0530

    add: base62Encoder

--- services/url-service/src/main/java/com/yashdotdev/url_service/generator/Base62Encoder.java
+++ services/url-service/src/main/java/com/yashdotdev/url_service/generator/Base62Encoder.java
@@ -1,4 +1,34 @@
 package com.yashdotdev.url_service.generator;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Base62Encoder {
+
+    private static final char[] CHARACTERS =
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                    .toCharArray();
+
+    private static final int BASE = CHARACTERS.length;
+
+    public String encode(long value) {
+
+        if (value == 0) {
+            return "0";
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        while (value > 0) {
+
+            int remainder = (int) (value % BASE);
+
+            builder.append(CHARACTERS[remainder]);
+
+            value /= BASE;
+        }
+
+        return builder.reverse().toString();
+    }
 }
--- services/url-service/src/main/java/com/yashdotdev/url_service/generator/ShortCodeGenerator.java
+++ services/url-service/src/main/java/com/yashdotdev/url_service/generator/ShortCodeGenerator.java
@@ -1,4 +1,20 @@
 package com.yashdotdev.url_service.generator;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class ShortCodeGenerator {
+
+    private final IdGenerator idGenerator;
+
+    private final Base62Encoder base62Encoder;
+
+    public String generate() {
+
+        long id = idGenerator.nextId();
+        return base62Encoder.encode(id);
+    }
+
 }
