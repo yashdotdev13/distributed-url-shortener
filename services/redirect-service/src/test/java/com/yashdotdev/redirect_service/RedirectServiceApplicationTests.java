167a6cd146051cfc9f1befc5ee7c93e82a18c7d1
98001yash <yashchauhan.gaya@gmail.com>
2026-07-23 22:05:00 +0530

    add: redirect-mioroservice

---
+++ services/redirect-service/src/test/java/com/yashdotdev/redirect_service/RedirectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.redirect_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
