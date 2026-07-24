47ad8170099cfe85d496714723ae96cdf6550f06
98001yash <yashchauhan.gaya@gmail.com>
2026-07-24 23:36:44 +0530

    add: analytic-service

---
+++ services/analytic-service/src/main/java/com/yashdotdev/analytic_service/AnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.analytic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticServiceApplication.class, args);
+	}
+
+}
---
+++ services/analytic-service/src/test/java/com/yashdotdev/analytic_service/AnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.analytic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
