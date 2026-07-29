1c29d9e803ae98ed8218a92261827fb1d9680b4e
98001yash <yashchauhan.gaya@gmail.com>
2026-07-29 15:27:44 +0530

    redirect-service added to the docker-compose

--- services/redirect-service/src/main/java/com/yashdotdev/redirect_service/config/KafkaProducerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yashdotdev.redirect_service.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class KafkaProducerConfig {
-}
